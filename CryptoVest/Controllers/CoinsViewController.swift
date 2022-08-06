fcbfce619ecfd173bb21847354fbaa73f12a4b45
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-06 21:19:57 +0300

    Add data source to TableView

--- CryptoVest/Controllers/CoinsViewController.swift
+++ CryptoVest/Controllers/CoinsViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class CoinsViewController: UIViewController {
-
+    private var coins = Coin.getCoins()
     @IBOutlet private var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +26,15 @@ extension CoinsViewController: UITableViewDelegate {
 
 extension CoinsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        coins.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell =  tableView.dequeueReusableCell(withIdentifier: "CryptoCoinTableViewCell", for: indexPath) as? CryptoCoinTableViewCell else { return UITableViewCell() }
+        cell.setCoin(coin: coins[indexPath.row])
+        if indexPath.row % 2 == 1 {
+            cell.backgroundColor = .placeholderText
+        }
         return cell
     }
     
