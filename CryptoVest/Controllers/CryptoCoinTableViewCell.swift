b82f0ecc9b0d6d746bce1c3cb8a0a65a7c8568d7
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-06 21:19:18 +0300

    Update CryptoCoinTableViewCell.swift
    make final class,  add UIView extension to rounded corners

--- CryptoVest/Controllers/CryptoCoinTableViewCell.swift
+++ CryptoVest/Controllers/CryptoCoinTableViewCell.swift
@@ -7,17 +7,47 @@
 
 import UIKit
 
-class CryptoCoinTableViewCell: UITableViewCell {
+final class CryptoCoinTableViewCell: UITableViewCell {
+   
+    @IBOutlet private var coinImage: UIImageView!
+    @IBOutlet private var coinIDLabel: UILabel!
+    @IBOutlet private var coinName: UILabel!
+    @IBOutlet private var changeLabel: UILabel!
+    @IBOutlet private var changeView: UIView!
+    @IBOutlet private var changeArrowImage: UIImageView!
+    @IBOutlet private var priceLabel: UILabel!
+   
+}
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+extension CryptoCoinTableViewCell {
+    func setCoin(coin: Coin) {
+        var arrowImage: UIImage
+        coinImage.image = UIImage(named: coin.coinID)
+        coinIDLabel.text = coin.coinID
+        coinName.text = coin.name
+        if coin.change >= 0 {
+            changeView.backgroundColor = .systemGreen
+            arrowImage = UIImage(named: "ArrowUp")!
+            arrowImage = arrowImage.withTintColor(.green)
+        } else {
+            changeView.backgroundColor = .red
+            arrowImage = UIImage(named: "ArrowDown")!
+            arrowImage = arrowImage.withTintColor(.red)
+        }
+        changeView.magicallySetCornerRadius()
+        changeArrowImage.image = arrowImage
+        changeLabel.text = "   \(abs(coin.change))%  "
+        priceLabel.text = "$\(coin.price)"
+    
     }
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+extension UIView {
+    /**
+     Magically computes and sets an ideal corner radius.
+     */
+    public func magicallySetCornerRadius() {
+        layer.cornerRadius = 0.188 * min(frame.width, frame.height)
+        layer.masksToBounds = true
     }
-
 }
