a90af6e8167e5b73b42218eaf0526c7cbdbd63d0
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-08 12:54:23 +0300

    add more coins

--- CryptoVest/Models/Coin.swift
+++ CryptoVest/Models/Coin.swift
@@ -19,6 +19,16 @@ extension Coin {
             Coin(coinID: "BCN", name: "Bytecoin", price: 32.48  , change: 4.72),
             Coin(coinID: "DASH", name: "Dash", price: 89.07 , change: 11.36),
             Coin(coinID: "OMG", name: "OmiceCo", price: 21.19 , change: -1.98),
+            Coin(coinID: "BTC", name: "Bitcoin", price: 17487.56 , change: 2.16),
+            Coin(coinID: "ETH", name: "Ethereum", price: 21.56 , change: -1.98),
+            Coin(coinID: "BCN", name: "Bytecoin", price: 32.48  , change: 4.72),
+            Coin(coinID: "DASH", name: "Dash", price: 89.07 , change: 11.36),
+            Coin(coinID: "OMG", name: "OmiceCo", price: 21.19 , change: -1.98),
+            Coin(coinID: "BTC", name: "Bitcoin", price: 17487.56 , change: 2.16),
+            Coin(coinID: "ETH", name: "Ethereum", price: 21.56 , change: -1.98),
+            Coin(coinID: "BCN", name: "Bytecoin", price: 32.48  , change: 4.72),
+            Coin(coinID: "DASH", name: "Dash", price: 89.07 , change: 11.36),
+            Coin(coinID: "OMG", name: "OmiceCo", price: 21.19 , change: -1.98),
             Coin(coinID: "PRC", name: "Peercoin", price: 129.89 , change: 23.8)
         ]
     }
