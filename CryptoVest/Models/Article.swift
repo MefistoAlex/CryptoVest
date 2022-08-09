1a324eef9680be1c15ac37c7e910dd101a27d9c5
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-09 16:07:04 +0300

    add calculated var publicised

--- CryptoVest/Models/Article.swift
+++ CryptoVest/Models/Article.swift
@@ -13,6 +13,31 @@ struct Article {
     let image: String
 }
 extension Article {
+    var publicised: String {
+        let seconds = date.timeIntervalSinceNow
+        // if time interval < 1 min
+        if seconds < 60 {
+            return "just now"
+        }
+        
+        // if time interval < 1 hour
+        if seconds < 60 * 60 {
+            return "\(Int(seconds / 60)) mins ago"
+        }
+        
+        // if time interval < 1 day
+        if seconds < 60 * 60 * 24 {
+            return "\(Int(seconds / (60 * 24))) hours ago"
+        }
+        
+        // if time interval < 10 days
+        if seconds < 60 * 60 * 24 * 10 {
+            return "\(Int(seconds / (60 * 24 * 10 ))) days ago"
+        }
+        
+        return date.description
+    }
+    
     func getNews() -> [Article] {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
