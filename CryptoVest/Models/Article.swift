380ae56a30cf2c6d794c64465955f8f693eefbbb
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-09 15:07:51 +0300

    create model Article

---
+++ CryptoVest/Models/Article.swift
@@ -0,0 +1,71 @@
+//
+//  Article.swift
+//  CryptoVest
+//
+//  Created by Alexandr Mefisto on 09.08.2022.
+//
+
+import Foundation
+struct Article {
+    let header: String
+    let authror: String
+    let date: Date
+    let image: String
+}
+extension Article {
+    func getNews() -> [Article] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssZZZ"
+        dateFormatter.date(from: "2022'-'08'-'07'T'12':'50':'00000")
+        return [
+            Article(
+                header: "Krypton Capital Founder Considers Legal Action Against Jamie Dimon",
+                authror: "Christine Masters",
+                date: dateFormatter.date(from: "2022'-'08'-'07'T'12':'50':'00000")!,
+                image: "article0"
+            ),
+            Article(
+                header: "British Cryptocurrency Electroneum Suffers Cyber Attack Locking Out Investors",
+                authror: "C. Masters",
+                date: dateFormatter.date(from: "2022'-'08'-'05'T'12':'50':'00000")!,
+                image: "article1"
+            ),
+            Article(
+                header: "To Regulate or Not? Securities Commission in Malaysia Talk Up Cryptocurrency Framework",
+                authror: "A. Antonovici",
+                date: dateFormatter.date(from: "2022'-'08'-'04'T'12':'50':'00000")!,
+                image: "article2"
+            ),
+            Article(
+                header: "Kik Chat ICO Draws Top VCs; We Carefully Review Why...",
+                authror: "H. Naseer",
+                date: dateFormatter.date(from: "2022'-'08'-'03'T'12':'50':'00000")!,
+                image: "article3"
+            ),
+            Article(
+                header: "Deloitte Lists Key Principles for Blockchain Adoption in Financial Sector",
+                authror: "T. DeSue",
+                date: dateFormatter.date(from: "2022'-'08'-'02'T'12':'50':'00000")!,
+                image: "article4"
+            ),
+            Article(
+                header: "Wharton MBA: Blockchain Course Set for Spring 2018",
+                authror: "M. Gomez",
+                date: dateFormatter.date(from: "2022'-'08'-'01'T'12':'50':'00000")!,
+                image: "article5"
+            ),
+            Article(
+                header: "China Crypto Ban Continues to Boost Bitcoin OTC Market Activity",
+                authror: "C. Masters",
+                date: dateFormatter.date(from: "2022'-'07'-'30'T'12':'50':'00000")!,
+                image: "article6"
+            ),
+            Article(
+                header: "Vatican in Discussions to Address Crypto Use in Human Trafficking",
+                authror: "H. Nasser",
+                date: dateFormatter.date(from: "2022'-'07'-'29'T'12':'50':'00000")!,
+                image: "article7"
+            )
+        ]
+    }
+}
