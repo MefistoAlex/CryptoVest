5776fd6cf1b2ce47fdd6ed8e9d18d60ca5ecac7e
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-09 16:24:17 +0300

    Create ArticleTableViewCell.swift

---
+++ CryptoVest/Controllers/ArticleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleTableViewCell.swift
+//  CryptoVest
+//
+//  Created by Alexandr Mefisto on 09.08.2022.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet var articleHeader: UILabel!
+    @IBOutlet var authorAndDate: UILabel!
+    @IBOutlet var articleImage: UIImageView!
+   
+}
+extension ArticleTableViewCell {
+    func setArticle(article: Article) {
+        articleHeader.text = article.header
+        authorAndDate.text = "\(article.authror), \(article.publicised)"
+        articleImage.image = UIImage(named: article.image)
+    }
+}
