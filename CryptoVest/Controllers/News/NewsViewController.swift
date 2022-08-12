4defa01c37e4d4302f0d16381511b794b02eeb85
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-12 14:21:53 +0300

    add top coin cell

--- CryptoVest/Controllers/News/NewsViewController.swift
+++ CryptoVest/Controllers/News/NewsViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class NewsViewController: UIViewController {
+    private let coins = Coin.getCoins()
     private let articles = Article.getNews()
     @IBOutlet var tableView: UITableView!
 
@@ -21,12 +22,16 @@ final class NewsViewController: UIViewController {
 
 extension NewsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        articles.count
+//        articles.count
+        1
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Article", for: indexPath) as! ArticleTableViewCell
-        cell.setArticle(article: articles[indexPath.row])
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as! ArticleTableViewCell
+//        cell.setArticle(article: articles[indexPath.row])
+//        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopCoinsTableViewCell", for: indexPath) as! TopCoinsTableViewCell
+        cell.setCoins(coins: coins)
         return cell
     }
 }
---
+++ CryptoVest/Controllers/News/TopCoinsTableViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  TopCoinsTableViewCell.swift
+//  CryptoVest
+//
+//  Created by Alexandr Mefisto on 12.08.2022.
+//
+
+import UIKit
+
+class TopCoinsTableViewCell: UITableViewCell {
+    private var coinsCount = 3
+   
+   
+    @IBOutlet var changeValues: [UILabel]!
+    @IBOutlet var changeViews: [UIView]!
+    @IBOutlet var arrows: [UIImageView]!
+    @IBOutlet var prices: [UILabel]!
+    @IBOutlet var coinsIDs: [UILabel]!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+extension TopCoinsTableViewCell {
+
+    func setCoins(coins: [Coin]) {
+        var arrowImage: UIImage?
+        coinsCount = coins.count <= coinsCount ? coins.count : coinsCount
+        
+        for index in 0..<coinsCount {
+            coinsIDs[index].text = coins[index].coinID
+
+            if coins[index].change >= 0 {
+                changeViews[index].setGradientBackground(
+                    colorTop: UIColor(named: CustomColors.greenFrom.rawValue),
+                    colorBottom: UIColor(named: CustomColors.greenTo.rawValue)
+                )
+                arrowImage = UIImage(named: CustomImages.arrowUp.rawValue)
+            } else {
+                changeViews[index].setGradientBackground(
+                    colorTop: UIColor(named: CustomColors.redFrom.rawValue),
+                    colorBottom: UIColor(named: CustomColors.redTo.rawValue))
+                arrowImage = UIImage(named: CustomImages.arrowDown.rawValue)
+            }
+
+            arrows[index].image = arrowImage
+            changeValues[index].text = "\(abs(coins[index].change))%"
+//            changeView.setCornerRadius()
+
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            formatter.maximumFractionDigits = 2
+            formatter.currencyDecimalSeparator = ","
+
+            prices[index].text = "$" + formatter.string(from: NSNumber(value: coins[index].price))!
+            
+        }
+    }
+}
