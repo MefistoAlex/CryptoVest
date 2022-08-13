889244270b1b2ad23b75a5439864bc756c9d6726
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-13 10:36:44 +0300

    bug finded ned to fix

--- CryptoVest/Extensions/Extensions.swift
+++ CryptoVest/Extensions/Extensions.swift
@@ -38,6 +38,8 @@ extension UIViewController {
 }
 
 extension UIView {
+    //TODO: fix gradient Bug
+    
     func setGradientBackground(colorTop: UIColor?, colorBottom: UIColor?) {
         guard let colorTop = colorTop, let colorBottom = colorBottom else { return }
 
