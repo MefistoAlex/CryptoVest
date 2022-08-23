973c6f3bad0098f5a964a270b9fefa74d99348e9
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-08-23 14:59:16 +0300

    added loading logos

--- CryptoVest/Services/CoinAPIService.swift
+++ CryptoVest/Services/CoinAPIService.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 protocol CoinAPIServiceInterface {
-    func getCoins(path: CoinsPaths, parameters: CoinRequestParameters?, completion: @escaping (_ responce: CoinRequestResponce?, _ error: Error?) -> Void)
+    func getCoins(path: CoinsPaths, parameters: CoinRequestParameters?, completion: @escaping (_ coins: [Coin]?, _ error: Error?) -> Void)
 
     func getCoins(path: CoinsPaths,
-                  completion: @escaping (CoinRequestResponce?, Error?) -> Void)
+                  completion: @escaping ([Coin]?, Error?) -> Void)
+
+    func getCoinLogos(path: CoinsPaths, coinsList: [String: String],
+                      completion: @escaping (CoinLogoRequestResponce?, Error?) -> Void)
 }
 
 final class CoinAPIService: CoinAPIServiceInterface {
@@ -21,19 +24,57 @@ final class CoinAPIService: CoinAPIServiceInterface {
     }
 
     func getCoins(path: CoinsPaths, parameters: CoinRequestParameters?,
-                  completion: @escaping (CoinRequestResponce?, Error?) -> Void) {
+                  completion: @escaping ([Coin]?, Error?) -> Void) {
         let parameters: CoinRequestParameters = parameters ?? CoinRequestParameters()
         apiManager.request(urlString: path.url,
                            method: .get,
                            dataType: CoinRequestResponce.self,
                            headers: CoinsPaths.headers,
                            parameters: parameters.toDictionary) { data, error in
-            completion(data, error)
+            var resultCoins: [Coin]?
+            if let coins = data?.data {
+                let coinsID = coins.map { String($0.id) }
+                    .joined(separator: ",")
+
+                self.getCoinLogos(path: .info, coinsList: ["id": coinsID]) { [self] data, error in
+
+                    if let data = data {
+                        let coinLogos = data.data
+                        resultCoins = self.setLogos(coins: coins, logos: coinLogos)
+                    } else {
+                        resultCoins = coins
+                    }
+                    completion(resultCoins, error)
+                }
+            } else {
+                completion(data?.data, error)
+            }
         }
     }
 
     func getCoins(path: CoinsPaths,
-                  completion: @escaping (CoinRequestResponce?, Error?) -> Void) {
+                  completion: @escaping ([Coin]?, Error?) -> Void) {
         getCoins(path: path, parameters: nil, completion: completion)
+    }
+
+    func getCoinLogos(path: CoinsPaths, coinsList: [String: String],
+                      completion: @escaping (CoinLogoRequestResponce?, Error?) -> Void) {
+        apiManager.request(urlString: path.url,
+                           method: .get,
+                           dataType: CoinLogoRequestResponce.self,
+                           headers: CoinsPaths.headers,
+                           parameters: coinsList) { data, error in
+            completion(data, error)
+        }
+    }
+
+    private func setLogos(coins: [Coin], logos: [Int: CoinLogo]) -> [Coin] {
+        var resultCoins = [Coin]()
+        for coin in coins {
+            var newCoin = coin
+            newCoin.logo = logos[newCoin.id]?.logo
+            resultCoins.append(newCoin)
+        }
+        return resultCoins
     }
 }
