d7cb11ed5d98abd45794da14e834451f808f7cc0
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-09-11 11:57:29 +0300

    make class final and loadNib() private

--- CryptoVest/Controllers/News/TableViewHeader/ArticleTableViewHeader.swift
+++ CryptoVest/Controllers/News/TableViewHeader/ArticleTableViewHeader.swift
@@ -5,36 +5,37 @@
 //  Created by Alexandr Mefisto on 12.08.2022.
 //
 
+import SDWebImage
 import UIKit
 
-class ArticleTableViewHeader: UIView {
-
+final class ArticleTableViewHeader: UIView {
     @IBOutlet var contentView: ArticleTableViewHeader!
     @IBOutlet var authorLabel: UILabel!
     @IBOutlet var articleHeaderLabel: UILabel!
     @IBOutlet var articleImageView: UIImageView!
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         loadNib()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadNib()
-//        fatalError("init(coder:) has not been implemented")
     }
-    
-    func loadNib() {
+
+    private func loadNib() {
         Bundle.main.loadNibNamed("ArticleTableViewHeader", owner: self, options: nil)
         addSubview(contentView)
         contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        contentView.frame = self.bounds
+        contentView.frame = bounds
     }
+
     func setArticle(article: Article) {
-        
         articleHeaderLabel.text = article.title
-        authorLabel.text = "\(article.authror), \(article.publicised)"
-        articleImageView.image = UIImage(named: article.image)
+        authorLabel.text = "\(article.author ?? "") \(article.publicised)"
+        if let imageUrl = article.urlToImage {
+            articleImageView.sd_setImage(with: URL(string: imageUrl), placeholderImage: UIImage(named: "welcome screen"))
+        }
     }
 }
