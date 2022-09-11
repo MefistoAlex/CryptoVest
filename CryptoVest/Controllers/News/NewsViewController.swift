e4a58a375f7ea3b2622f5c25b61a2d8b8ace22f9
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-09-11 12:01:38 +0300

    add news loading from API

--- CryptoVest/Controllers/News/NewsViewController.swift
+++ CryptoVest/Controllers/News/NewsViewController.swift
@@ -8,25 +8,47 @@
 import UIKit
 
 final class NewsViewController: UIViewController {
-    private let coins:[Coin] = []
-    private let articles = Article.getNews()
+    private var coins: [Coin] = []
+    private var articles: [Article] = []
     @IBOutlet var tableView: UITableView!
+
+    private lazy var coinService: CoinAPIServiceInterface = {
+        CoinAPIService()
+    }()
+
+    private lazy var newsService: NewsAPIServiceInterface = {
+        NewsAPIService()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
         setHeader()
-       
-        
+        coinService.getCoins(path: .latests) { coins, _ in
+            if let coins = coins {
+                self.coins = coins
+            }
+        }
+        newsService.getNews { articles, _ in
+            if let articles = articles {
+                self.articles = articles
+            }
+            self.coinService.getCoins(path: .latests) { coins, _ in
+                if let coins = coins {
+                    self.coins = coins
+                }
+                self.tableView.reloadData()
+            }
+        }
     }
-  
 }
 
 extension NewsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         0.66 * view.bounds.height
     }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         articles.count
     }
@@ -38,7 +60,7 @@ extension NewsViewController: UITableViewDataSource {
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as! ArticleTableViewCell
-            cell.setArticle(article: articles[indexPath.row] )
+            cell.setArticle(article: articles[indexPath.row])
             return cell
         }
     }
@@ -48,9 +70,12 @@ extension NewsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
+
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = ArticleTableViewHeader()
-        headerView.setArticle(article: articles[0])
+        if articles.count > 0 {
+            headerView.setArticle(article: articles[0])
+        }
         return headerView
     }
 }
