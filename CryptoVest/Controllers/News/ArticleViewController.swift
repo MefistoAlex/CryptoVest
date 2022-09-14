b67a4c69046efb507c8095899da47b0202fcb049
Mefisto Alex <Alexandr.mefisto@gmail.com>
2022-09-14 16:03:02 +0300

    add article view controller

---
+++ CryptoVest/Controllers/News/ArticleViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleViewController.swift
+//  CryptoVest
+//
+//  Created by Alexandr Mefisto on 12.09.2022.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
